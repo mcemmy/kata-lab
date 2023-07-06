c2d314a88ddddca30641070a0c7fd343d98a566d
mcemmy <mcemmy89@gmail.com>
2023-07-06 21:43:06 +0400

    divisible by 15

--- fizz-buzz/go/fizz-buzz.go
+++ fizz-buzz/go/fizz-buzz.go
@@ -17,7 +17,8 @@ func FizzBuzz() []string {
 
 	result := make([]string, 100)
 	for i := 1; i <= 100; i++ {
-		if i%3 == 0 && i%5 == 0 {
+		if i%15 == 0 {
+			//divisible by 15 is divisible by 3 and 5
 			result[i-1] = "FizzBuzz"
 			continue
 		}
