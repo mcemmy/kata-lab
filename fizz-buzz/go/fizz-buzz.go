47c451f31183716f1967dd9add74c514e73ade6e
mcemmy <mcemmy89@gmail.com>
2023-07-06 21:41:46 +0400

    multiple of 3, 5

--- fizz-buzz/go/fizz-buzz.go
+++ fizz-buzz/go/fizz-buzz.go
@@ -3,18 +3,34 @@ package main
 import "fmt"
 
 func main() {
+	var numbers = FizzBuzz()
 	fmt.Println("Hello, Fizz-buzz!")
+	fmt.Println("********************")
+	fmt.Println(numbers)
 }
 
 //Write a program that prints the numbers from 1 to 100.
 //But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".
 //For numbers which are multiples of both three and five print "FizzBuzz".
 
-func FizzBuzz() []int {
+func FizzBuzz() []string {
 
-	result := make([]int, 100)
+	result := make([]string, 100)
 	for i := 1; i <= 100; i++ {
-		result[i-1] = i
+		if i%3 == 0 && i%5 == 0 {
+			result[i-1] = "FizzBuzz"
+			continue
+		}
+		if i%3 == 0 {
+			result[i-1] = "Fizz"
+			continue
+		} else if i%5 == 0 {
+			result[i-1] = "Buzz"
+			continue
+		} else {
+			result[i-1] = fmt.Sprint(i)
+		}
+
 	}
 	return result
 }
--- fizz-buzz/go/fizz-buzz_test.go
+++ fizz-buzz/go/fizz-buzz_test.go
@@ -3,7 +3,8 @@ package main
 import "testing"
 
 //Write a program that prints the numbers from 1 to 100.
-//But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".
+//But for multiples of three print "Fizz" instead of the number
+//and for the multiples of five print "Buzz".
 //For numbers which are multiples of both three and five print "FizzBuzz".
 func TestPrintsNumberFromOneToHundred(t *testing.T) {
 	result := FizzBuzz()
@@ -13,11 +14,58 @@ func TestPrintsNumberFromOneToHundred(t *testing.T) {
 		t.Errorf("Expected 100 results, got %d", len(result))
 	}
 
-	if result[0] != 1 {
-		t.Errorf("Expected 1, got %d", result[0])
+	if result[0] != "1" {
+		t.Errorf("Expected 1, got %s", result[0])
 	}
+}
 
-	if result[99] != 100 {
-		t.Errorf("Expected 100, got %d", result[99])
+func TestPrintsFizzForMultiplesOfThree(t *testing.T) {
+	result := FizzBuzz()
+	if result[2] != "Fizz" {
+		t.Errorf("Expected Fizz, got %s", result[2])
+	}
+	if result[5] != "Fizz" {
+		t.Errorf("Expected Fizz, got %s", result[5])
+	}
+	if result[98] != "Fizz" {
+		t.Errorf("Expected Fizz, got %s", result[98])
+	}
+}
+
+func TestPrintsBuzzForMultiplesOfFive(t *testing.T) {
+	result := FizzBuzz()
+	if result[4] != "Buzz" {
+		t.Errorf("Expected Buzz, got %s", result[4])
+	}
+	if result[9] != "Buzz" {
+		t.Errorf("Expected Buzz, got %s", result[9])
+	}
+	if result[19] != "Buzz" {
+		t.Errorf("Expected Buzz, got %s", result[19])
+	}
+	if result[99] != "Buzz" {
+		t.Errorf("Expected Buzz, got %s", result[99])
+	}
+}
+
+func TestPrintsFizzBuzzForMultiplesOfThreeAndFive(t *testing.T) {
+	result := FizzBuzz()
+	if result[14] != "FizzBuzz" {
+		t.Errorf("Expected FizzBuzz, got %s", result[14])
+	}
+	if result[29] != "FizzBuzz" {
+		t.Errorf("Expected FizzBuzz, got %s", result[29])
+	}
+	if result[44] != "FizzBuzz" {
+		t.Errorf("Expected FizzBuzz, got %s", result[44])
+	}
+	if result[59] != "FizzBuzz" {
+		t.Errorf("Expected FizzBuzz, got %s", result[59])
+	}
+	if result[74] != "FizzBuzz" {
+		t.Errorf("Expected FizzBuzz, got %s", result[74])
+	}
+	if result[89] != "FizzBuzz" {
+		t.Errorf("Expected FizzBuzz, got %s", result[89])
 	}
 }
